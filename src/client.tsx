0f0ee48a8b20d13c1656ebdf7c064236b2a8e483
Matías <mjlescano@protonmail.com>
2023-02-15 11:21:59 +0100

    fix client build

--- src/client.tsx
+++ src/client.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import { hydrate } from 'react-dom';
 
 import App from './App';
 
-const root = createRoot(document.getElementById('root'));
-
-root.render(
+hydrate(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 if (module.hot) {
