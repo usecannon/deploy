6de78db00d64e581762883ecb5f72fff8af3e3c0
Matías <mjlescano@protonmail.com>
2023-03-06 19:06:29 +0100

    add PoC building step on Cannon component

--- src/Cannon.tsx
+++ src/Cannon.tsx
@@ -1,54 +1,143 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useMemo } from 'react'
+import { ethers, providers } from 'ethers';
+import {
+  build,
+  CannonWrapperGenericProvider,
+  ChainBuilderRuntime,
+  ChainDefinition,
+  createInitialContext,
+  IPFSLoader,
+  OnChainRegistry
+} from '@usecannon/builder'
 import { useSafeAppsSDK } from '@safe-global/safe-apps-react-sdk'
-import { IPFSLoader, OnChainRegistry } from '@usecannon/builder/dist/cannon.umd'
+import { SafeAppProvider } from '@safe-global/safe-apps-provider';
 
-// TODO: move to dynamic
-const IPFS_URL = 'https://usecannon.infura-ipfs.io'
+// TODO: move to dynamic env bars
 const registryProviderUrl = 'https://mainnet.infura.io/v3/4791c1745a1f44ce831e94be7f9e8bd7'
 const registryAddress = '0x8E5C7EFC9636A6A0408A46BB7F617094B81e5dba'
 
-// has one text box and one button
+const IPFS_URL = 'https://usecannon.infura-ipfs.io'
+
+class CannonSafeAppProvider extends SafeAppProvider {
+  async request({ method, params }) {
+    console.log({ method, params }, 1)
+    const res = await super.request({ method, params })
+    console.log({ method, params }, res)
+    return res
+  }
+}
+
 const Cannon = (): React.ReactElement => {
-  const { safe } = useSafeAppsSDK()
+  const [deployStatus, setDeployStatus] = useState<string>('loading...')
+  const [errorMessage, setErrorMessage] = useState<string>('')
+
+  const { safe, connected, sdk } = useSafeAppsSDK()
   const { chainId } = safe
 
-  const [deployInfo, setDeployInfo] = useState<string>('')
+  const web3Provider = useMemo(() => {
+    const cannonProvider = new CannonSafeAppProvider(safe, sdk)
+    return new ethers.providers.Web3Provider(cannonProvider)
+  }, [sdk, safe]);
 
-  // const chainId = 13370
+  // TODO: move to form inputs
   const preset = 'main'
-  const packageName = 'synthetix:latest'
+  const packageUrl = '@ipfs:QmPUNRGSuZVGvsuYycH62c1tYzswtNQS1dViYHZavTXmhH'
+  const upgradeFrom = '@ipfs:QmPASEogqe59LaLv5Htd95tzqPyM4e5yuC9qJJ4iA9sjtq' // 'synthetix:latest'
 
-  useEffect(() => {
-    const getDeploy = async () => {
+  const getDeploy = async () => {
+    try {
+      /*
+      const runtime = new ChainBuilderRuntime({...}, new IPFSLoader(..., safe.provider));
+      await runtime.restoreMisc(unfinishedDeployData.miscUrl);
+      const def = new ChainDefinition(unfinishedDeployData.def);
+      // some config stuff
+      const newState = await cannonBuild(runtime, def, unfinishedDeployData.state : {}, initialCtx);
+      // ** magic **
+      // publish new state on ipfs
+      // done!
+      */
+
       const registry = new OnChainRegistry({
         signerOrProvider: registryProviderUrl,
         address: registryAddress,
       })
-
       const loader = new IPFSLoader(IPFS_URL, registry)
+      const previousDeploy = await loader.readDeploy(upgradeFrom, preset, chainId)
+      const incompleteDeploy = await loader.readDeploy(packageUrl, preset, chainId)
 
-      try {
-        const deployInfo = await loader.readDeploy(packageName, preset, chainId)
-
-        if (!deployInfo) {
-          throw new Error(`Package not found: ${packageName} (${chainId}-${preset})`)
-        }
-
-        console.log('deployInfo: ', deployInfo)
-        setDeployInfo(JSON.stringify(deployInfo, null, 2))
-      } catch (e) {
-        console.error(e)
+      if (!incompleteDeploy) {
+        throw new Error(`Package not found: ${packageUrl} (${chainId}-${preset})`)
       }
-    }
 
+      if (!previousDeploy) {
+        throw new Error(`Package not found: ${upgradeFrom} (${chainId}-${preset})`)
+      }
+
+      setDeployStatus(incompleteDeploy.status)
+
+      // TODO: Check if the downloaded deployment is from the same chainId.
+      // Cannon is not doing this for deployments loaded from @ipfs urls, but,
+      // we are currently not saving the chainId on the metadata, so I'm not sure how could we do it.
+
+      if (incompleteDeploy.status === 'complete') {
+        return setErrorMessage('Current deployment is already deployed completely')
+      }
+
+      if (previousDeploy.status !== 'complete') {
+        return setErrorMessage('Previous deployment is not deployed completely')
+      }
+
+      // TODO: check that the remaining steps to be executed do not depend on other not-executed steps
+      // as this would cause an error (or only execute until it can)
+
+      const provider = new CannonWrapperGenericProvider({}, web3Provider)
+
+      const runtime = new ChainBuilderRuntime({
+        provider,
+        chainId,
+        getSigner: async (addr: string) => provider.getSigner(addr),
+        baseDir: null,
+        snapshots: false,
+        allowPartialDeploy: false,
+        publicSourceCode: true,
+      }, loader);
+
+      await runtime.restoreMisc(previousDeploy.miscUrl);
+      const def = new ChainDefinition(previousDeploy.def);
+
+      const meta = {} // TODO: allow to add custom meta
+      const resolvedSettings = Object.assign(previousDeploy.options ?? {}, incompleteDeploy.options);
+      const initialCtx = await createInitialContext(def, meta, resolvedSettings);
+
+      setDeployStatus('building...')
+      const newState = await build(runtime, def, previousDeploy.state ?? {}, initialCtx);
+
+      console.log({ newState })
+
+      console.log('previousDeploy: ', previousDeploy)
+      console.log('incompleteDeploy: ', incompleteDeploy)
+
+      setDeployStatus('done!')
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  useEffect(() => {
+    if (!chainId) return
     getDeploy()
   }, [chainId])
 
-  return <div>
-    <p>chainId: {chainId}</p>
-    <p>Safe: {safe.safeAddress}</p>
-    <code>{deployInfo}</code>
-  </div>
+  if (!connected) return <p>Not connected to safe wallet</p>
+  if (errorMessage) return <p>{errorMessage}</p>
+
+  return (
+    <div>
+      <p>chainId: {chainId}</p>
+      <p>Safe: {safe.safeAddress}</p>
+      <p>Status: {deployStatus}</p>
+    </div>
+  )
 }
 
 export default Cannon
