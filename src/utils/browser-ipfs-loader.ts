2f380ac41cdd6d2468abee5cdce997d14dd473ea
Matías <mjlescano@protonmail.com>
2023-03-24 00:06:31 +0100

    fix ipfs with infura

--- src/utils/browser-ipfs-loader.ts
+++ src/utils/browser-ipfs-loader.ts
@@ -1,8 +1,66 @@
-import { DeploymentInfo, IPFSLoader } from '@usecannon/builder'
+// import { CannonRegistry, IPFSLoader } from '@usecannon/builder'
+// import type { Headers } from '@usecannon/builder/dist/ipfs'
+
+// export class IPFSBrowserLoader extends IPFSLoader {
+//   constructor(
+//     ipfsUrl: string,
+//     resolver: CannonRegistry,
+//     customHeaders: Headers = {}
+//   ) {
+//     super(ipfsUrl, resolver, customHeaders)
+
+//     const url = new URL(ipfsUrl)
+//     if (url.username || url.password) {
+//       this.customHeaders['Authorization'] = `Basic ${btoa(
+//         `${url.username}:${url.password}`
+//       )}`
+
+//       url.username = ''
+//       url.password = ''
+
+//       this.ipfsUrl = url.toString()
+//     }
+//   }
+// }
+
+import { CannonRegistry, DeploymentInfo, IPFSLoader } from '@usecannon/builder'
+import type { Headers } from '@usecannon/builder/dist/ipfs'
 import { IPFSHTTPClient, create } from 'ipfs-http-client'
 
 export class IPFSBrowserLoader extends IPFSLoader {
-  private client = create(this.ipfsUrl as unknown)
+  private client: IPFSHTTPClient
+
+  constructor(
+    ipfsUrl: string,
+    resolver: CannonRegistry,
+    customHeaders: Headers = {}
+  ) {
+    super(ipfsUrl, resolver, customHeaders)
+
+    const url = new URL(ipfsUrl)
+
+    if (url.username || url.password) {
+      this.customHeaders['Authorization'] = `Basic ${btoa(
+        `${url.username}:${url.password}`
+      )}`
+
+      this.client = create({
+        port: Number.parseInt(url.port),
+        host: url.hostname,
+        protocol: url.protocol.replace(':', ''),
+        headers: this.customHeaders as unknown as Record<string, string>,
+      })
+
+      url.username = ''
+      url.password = ''
+
+      this.ipfsUrl = url.toString()
+    } else {
+      this.client = create({
+        url: this.ipfsUrl,
+      })
+    }
+  }
 
   async putDeploy(deployInfo: DeploymentInfo) {
     const hash = await writeIpfs(this.client, deployInfo)
