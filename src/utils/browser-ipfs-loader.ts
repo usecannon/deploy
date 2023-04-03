d20c46d4490d33eb3acfaf59e57cda134b27b92e
Matías <mjlescano@protonmail.com>
2023-04-03 13:20:41 +0200

    fix ipfs upload

--- src/utils/browser-ipfs-loader.ts
+++ src/utils/browser-ipfs-loader.ts
@@ -1,20 +1,54 @@
-import { CannonRegistry, IPFSLoader } from '@usecannon/builder'
+import axios from 'axios'
+import pako from 'pako'
+import { DeploymentInfo, IPFSLoader } from '@usecannon/builder'
 import type { Headers } from '@usecannon/builder/dist/ipfs'
 import { create as createUrl, parse as parseUrl } from 'simple-url'
 
 export class IPFSBrowserLoader extends IPFSLoader {
-  constructor(
-    ipfsUrl: string,
-    resolver: CannonRegistry,
-    customHeaders: Headers = {}
-  ) {
-    const { url, headers } = createIpfsUrl(ipfsUrl, '')
-    super(url.replace(/\/$/, ''), resolver, { ...headers, ...customHeaders })
+  async putDeploy(deployInfo: DeploymentInfo) {
+    const hash = await _writeIpfs(
+      this.ipfsUrl,
+      JSON.stringify(deployInfo),
+      this.customHeaders
+    )
+    return hash ? `${IPFSLoader.PREFIX}${hash}` : hash
+  }
+
+  async putMisc(misc: unknown) {
+    const hash = await _writeIpfs(
+      this.ipfsUrl,
+      JSON.stringify(misc),
+      this.customHeaders
+    )
+    return hash ? `${IPFSLoader.PREFIX}${hash}` : hash
   }
 }
 
+async function _writeIpfs(
+  ipfsUrl: string,
+  content: string,
+  customHeaders: Headers = {}
+) {
+  if (!content) throw new Error('No content to upload')
+
+  const { url, headers } = _createIpfsUrl(ipfsUrl, '/api/v0/add')
+  const formData = new FormData()
+
+  const buff = pako.deflate(content)
+
+  formData.append('data', buff)
+
+  const res = await axios.post(url, formData, {
+    headers: { ...headers, ...customHeaders },
+  })
+
+  console.log('uploaded', res.statusText, res.data.Hash)
+
+  return res.data.Hash
+}
+
 // Create an ipfs url with compatibility for custom auth and https+ipfs:// protocol
-function createIpfsUrl(base: string, pathname: string) {
+function _createIpfsUrl(base: string, pathname: string) {
   const parsedUrl = parseUrl(base)
   const headers: { [k: string]: string } = {}
 
@@ -40,59 +74,3 @@ function createIpfsUrl(base: string, pathname: string) {
 
   return { url: createUrl(uri), headers }
 }
-
-// import { CannonRegistry, DeploymentInfo, IPFSLoader } from '@usecannon/builder'
-// import type { Headers } from '@usecannon/builder/dist/ipfs'
-// import { IPFSHTTPClient, create } from 'ipfs-http-client'
-
-// export class IPFSBrowserLoader extends IPFSLoader {
-//   private client: IPFSHTTPClient
-
-//   constructor(
-//     ipfsUrl: string,
-//     resolver: CannonRegistry,
-//     customHeaders: Headers = {}
-//   ) {
-//     super(ipfsUrl, resolver, customHeaders)
-
-//     const url = new URL(ipfsUrl)
-
-//     if (url.username || url.password) {
-//       this.customHeaders['Authorization'] = `Basic ${btoa(
-//         `${url.username}:${url.password}`
-//       )}`
-
-//       this.client = create({
-//         port: Number.parseInt(url.port),
-//         host: url.hostname,
-//         protocol: url.protocol.replace(':', ''),
-//         headers: this.customHeaders as unknown as Record<string, string>,
-//       })
-
-//       url.username = ''
-//       url.password = ''
-
-//       this.ipfsUrl = url.toString()
-//     } else {
-//       this.client = create({
-//         url: this.ipfsUrl,
-//       })
-//     }
-//   }
-
-//   async putDeploy(deployInfo: DeploymentInfo) {
-//     const hash = await writeIpfs(this.client, deployInfo)
-//     return hash ? `${IPFSLoader.PREFIX}${hash}` : hash
-//   }
-
-//   async putMisc(misc: unknown) {
-//     const hash = await writeIpfs(this.client, misc)
-//     return hash ? `${IPFSLoader.PREFIX}${hash}` : hash
-//   }
-// }
-
-// async function writeIpfs(client: IPFSHTTPClient, content: unknown) {
-//   const data = JSON.stringify(content)
-//   const { cid } = await client.add(data)
-//   return cid.toString()
-// }
