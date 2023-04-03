b4526af0bde1e1148df2554d9b4dfcc4dbac11e5
Matías <mjlescano@protonmail.com>
2023-04-03 12:39:57 +0200

    update netlify headers

--- .proxyrc.js
+++ .proxyrc.js
@@ -1,17 +1,18 @@
 const headers = {
   'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'GET',
-  'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+  'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+  'Access-Control-Allow-Headers':
+    'X-Requested-With, content-type, Authorization',
 }
 
 module.exports = function (app) {
   app.use(function (req, res, next) {
-    console.log(req.url);
+    console.log(req.url)
 
     for (const [key, val] of Object.entries(headers)) {
       res.setHeader(key, val)
     }
 
     next()
-  });
-};
+  })
+}
