6c5bcc5226db4257575d30f72173273643a5c8b6
Matías <mjlescano@protonmail.com>
2023-07-04 21:08:50 +0200

    lint backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -60,36 +60,39 @@ async function start() {
 
   app.post('/:chainId/:safeAddress', async (req, res) => {
     try {
-
       const signedTransactionInfo: StagedTransaction = req.body
 
       const chainId = parseInt(req.params.chainId)
-  
+
       const provider = providers.get(BigInt(chainId))
-  
+
       if (!provider) {
         return res.status(400).send('chain id not supported')
       }
-  
-      const safe = new ethers.Contract(req.params.safeAddress, SafeABI, provider)
-  
+
+      const safe = new ethers.Contract(
+        req.params.safeAddress,
+        SafeABI,
+        provider
+      )
+
       const txs = txdb.get(getSafeKey(chainId, req.params.safeAddress)) || []
-  
+
       const existingTx = txs.find(
         (tx) => JSON.stringify(tx.txn) == JSON.stringify(signedTransactionInfo)
       )
-  
+
       const currentNonce: bigint = await safe.nonce()
-  
+
       if (!existingTx) {
         // verify the new txn will work on what we know about the safe right now
-  
+
         if (signedTransactionInfo.txn._nonce < currentNonce) {
           return res
             .status(400)
             .send('proposed nonce is lower than current safe nonce')
         }
-  
+
         if (
           signedTransactionInfo.txn._nonce > currentNonce &&
           !txs.find(
@@ -110,7 +113,7 @@ async function start() {
             .send('new sigs count must be greater than old sigs count')
         }
       }
-  
+
       // verify all sigs are valid
       const hashData = await safe.encodeTransactionData(
         signedTransactionInfo.txn.to,
@@ -124,7 +127,7 @@ async function start() {
         signedTransactionInfo.txn.refundReceiver,
         signedTransactionInfo.txn._nonce
       )
-  
+
       try {
         await safe.checkNSignatures(
           ethers.keccak256(hashData),
@@ -136,15 +139,15 @@ async function start() {
         console.log('failed checking n signatures', err)
         return res.status(400).send('invalid signature')
       }
-  
+
       txs.push(signedTransactionInfo)
-  
+
       txdb.set(
         getSafeKey(chainId, req.params.safeAddress),
         // briefly clean up any txns that are less than current nonce
         txs.filter((t) => t.txn._nonce >= currentNonce)
       )
-  
+
       res.send(txs)
     } catch (err) {
       console.error('caught failure in transaction post', err)
